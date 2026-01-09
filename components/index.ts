8c34143334891d8865d3774e9b0d731596983df5
alihassan9682 <alihassan9682@gmail.com>
2026-01-09 12:36:34 +0500

    fix: fixed build issue

--- components/index.ts
+++ components/index.ts
@@ -3,6 +3,6 @@ import CompanyOverview from './CompanyOverview';
 import HeroCarousel from './HeroCarousel';
 import FeaturesAndBrands from './FeaturesAndBrands';
 import TestimonialSlider from "./TestimonialSlider"
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 import Footer from "./Footer";
 export { CallToAction, CompanyOverview, HeroCarousel, FeaturesAndBrands, TestimonialSlider, Navbar, Footer };
